ed20d71b7a9f5c7206828ececb52c1f192c4b830
Vladyslav Sitalo <sitalov@amazon.com>
2020-01-27 22:03:30 -0800

    Add support for recurring dates.

--- README.md
+++ README.md
@@ -5,6 +5,21 @@ Made for use with http://roamresearch.com/
 
 ![](giflink.gif)
 
+Trigger via `fd` keyword (or shortcut that you can set in Alfred settings)
+
+### Use cases:
+* Parse fuzzy date - just enter the date.
+* If you want to get a repeated sequence of dates - enter the increment and then number of repeats after the `;`. Example: `1 week;4` would yield 4 dates starting a week from now and ending 5 weeks into the future
+
+![](giflink_repeated.gif)
+
+### Configurations
+* If you just want to copy the result without pasting it into the frontmost application - hold `Cmd`.
+* You can configure precise format of the output in the workflow environment variables.
+
+
+
 ## Installation
 
 `npm install -g alfred-fuzzy-date`
+
--- src/main/kotlin/org/stvad/fuzzy/FuzzyDate.kt
+++ src/main/kotlin/org/stvad/fuzzy/FuzzyDate.kt
@@ -5,16 +5,40 @@ import org.stvad.fuzzy.extlib.alfy
 import org.stvad.fuzzy.extlib.chrono
 import org.stvad.fuzzy.extlib.dateFormat
 import org.stvad.fuzzy.extlib.process
+import kotlin.js.Date
 
 fun main() {
-    alfy.output(listOf(fuzzyDate()))
+    alfy.output(listOfNotNull(fuzzyDate(), recurringDate()))
 }
 
-fun fuzzyDate(): AlfredResult {
-    val date = chrono.parseDate(alfy.input, options = object {
-        val forwardDate = true
-    }) ?: return AlfredResult("I don't know this one \uD83D\uDE3F")
+val chronoOptions = object {
+    val forwardDate = true
+}
 
-    val dateString = dateFormat(date, process.env.dateFormat as String)
+val extractedDate = chrono.parseDate(alfy.input, options = chronoOptions)
+
+fun fuzzyDate(): AlfredResult {
+    val date = extractedDate ?: return AlfredResult("I don't know this one \uD83D\uDE3F")
+
+    val dateString = formatDate(date)
     return AlfredResult(dateString, arg = dateString)
+}
+
+fun formatDate(date: Date) = dateFormat(date, process.env.dateFormat as String)
+
+fun recurringDate(): AlfredResult? {
+    extractedDate ?: return null
+
+    val parts = alfy.input.split(";")
+    if (parts.size < 2) return null
+
+    val repeats = parts[1].toIntOrNull() ?: return null
+
+    val dates = generateSequence(extractedDate) {
+        chrono.parseDate(alfy.input, it, chronoOptions)
+    }.take(repeats).map { formatDate(it) }
+
+    val dateString = dates.joinToString(process.env.repeatedSeparator)
+
+    return AlfredResult("Repeated", dates.joinToString(" "), arg = dateString)
 }
